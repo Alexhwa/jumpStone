f771a6a409da51f4668d230cf1c54a54e5e2b9ff
Luan Nguyen <luan000000@hotmail.com>
2016-03-09 20:32:02 -0800

    Initial commit

---
+++ desktop/src/com/quickbreak/jumpstone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.quickbreak.jumpstone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.quickbreak.jumpstone.JumpStone;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JumpStone(), config);
+	}
+}
